36b2e6569602c721c537c1ce80f4206f765998cd
Antonio Rivas <chainpeu@gmail.com>
2016-11-12 10:02:50 +0400

    fix on fireBaseConfigTest (add export)

--- src/config/firebaseConfig.test.ts
+++ src/config/firebaseConfig.test.ts
@@ -1,7 +1,7 @@
 /* create a file like this one with your own configuration. 
    File should be named firebaseConfig.ts
 */
-const firebaseConfigTest = {
+export const firebaseConfigTest = {
   apiKey: "YourAPIKey",
   authDomain: "yourAppURL",
   databaseURL: "YourAppDatabaseURL",
