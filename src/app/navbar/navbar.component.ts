0b9eabacdcd4ec442821b3e6a0ebd158b01738a9
Antonio Rivas <chainpeu@gmail.com>
2017-01-07 11:02:51 +0100

    fixed profile and navbar inconsistency

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,18 +8,10 @@ import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  
-  photoURL:string;
-  name:string='';  
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
-      this.authService.af.database.object('/users/' + this.authService.uid)
-        .subscribe((u) => {
-          this.photoURL = u.photoURL;
-          this.name = u.displayName;        
-    });
   }
 
   logout() {    
